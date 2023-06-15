41b3777e3e40dc27e7a4182633b0099407535460
Garrett Andrews <garrettjandrews@gmail.com>
2023-06-14 20:30:13 -0400

    initial commit with proper structure

---
+++ app.js
@@ -0,0 +1,28 @@
+// define reqs
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+mongoose.set('strictQuery', false)
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl).then(response => {
+    console.log("connected")
+}).catch(error => {
+    console.log("failed to connect")
+})
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.errorHandler)
+
+module.exports = app
